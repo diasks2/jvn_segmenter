1053d9b8ffd68628f28712f3c598ccd781d665a6
Kevin Dias <diasks2@gmail.com>
2014-08-28 23:13:53 +0900

    Remove unescape

--- server.rb
+++ server.rb
@@ -10,7 +10,7 @@ end
 
 post "/?" do
   return unless params[:key] == ENV['KEY']
-  text = CGI.unescape(params[:text])
+  text = params[:text]
   segmented_text = segmenter.segment text
   array = segmented_text.scan(/\[([^\]]*)\]/)
   Hash[(0...array.size).zip array].to_json
